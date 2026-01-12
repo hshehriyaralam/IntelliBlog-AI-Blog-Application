bcbc699b14c519ec1b8eff291b1782f47e1cfafa
Hafiz Shehriyar Alam <154017155+hshehriyaralam@users.noreply.github.com>
2026-01-12 23:37:58 +0500

    select webpack

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,25 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-    
-   images: {
+  images: {
     domains: [
-      'popupdomination.com',
-      'tse1.mm.bing.net',
-      'tse2.mm.bing.net',
-      'tse3.mm.bing.net',
-      'tse4.mm.bing.net',
-      'coderscoach.com',
-      'wallpaperaccess.com',
-      'content.altexsoft.com',
-      
+      "popupdomination.com",
+      "tse1.mm.bing.net",
+      "tse2.mm.bing.net",
+      "tse3.mm.bing.net",
+      "tse4.mm.bing.net",
+      "coderscoach.com",
+      "wallpaperaccess.com",
+      "content.altexsoft.com",
     ],
-  },  
+  },
+
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(process.cwd(), "src");
+    config.resolve.alias["@/app"] = path.resolve(process.cwd(), "src/app");
+    return config;
+  },
 };
 
 export default nextConfig;
